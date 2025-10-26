737f32bb34cf0decaac2d60b83c98770837bd4b4
rohit <147339389+Rohit3325@users.noreply.github.com>
2025-10-26 18:37:31 +0530

    header

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -8,61 +8,93 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from './ui/button';
-import {  LayoutDashboard } from 'lucide-react';
-
+import { LayoutDashboard , StarsIcon,FileText, PenBox, GraduationCap } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   return (
-    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 
-    supports-[backdrop-filter]:bg-background/60 '>
-        <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-            <Link href='/'>
-              <Image
-              src="/logo.png" 
-              alt="sangrah logo"  width={200} height={60}
-              className='h-12 py-1 w-auto object-contain'
-              /> 
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        {/* Logo - Left */}
+        <Link href='/'>
+          <Image
+            src="/logo.png"
+            alt="sangrah logo"
+            width={200}
+            height={60}
+            className='h-12 py-1 w-auto object-contain'
+          />
+        </Link>
+
+        {/* Buttons - Right */}
+        <div className=' flex items-center space-x-2 md:space-x-4'>
+         
+          <SignedIn>
+            <Link href={'/dashboard'}>
+              <Button className="rounded-lg" variant="outline">
+                <LayoutDashboard className='h-4 w-4' />
+                <span className='hidden md:block'>Industry Insights</span>
+              </Button>
             </Link>
-
-            <div>
-             <SignedIn>
-                <Link href={'/dashboard'}>
-                <Button>
-                   <LayoutDashboard className='h-4 w-4' />
-                   <span className='hidden md:block'>
-                    Industry Inshights
-                   </span>
-                     
-                </Button>
+         
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button className="rounded-lg bg-white text-black">
+                <StarsIcon className='h-4 w-4' />
+                <span className='hidden md:block'>Growth Tools</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={"/resume"} className="flex items-center gap-2">
+                 <FileText className='h-4 w-4' />
+                <span >Buil Resume</span>
                 </Link>
-             </SignedIn>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                 <Link href={"/ai-cover-letter"} className="flex items-center gap-2">
+                 <PenBox className='h-4 w-4' />
+                <span >Cover Letter</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                 <Link href={"/interview"} className="flex items-center gap-2">
+                 <GraduationCap className='h-4 w-4' />
+                <span >Interview Prep</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+           </SignedIn>
 
-             <DropdownMenu>
-              <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-              </DropdownMenuContent>
-               </DropdownMenu>            
-            </div>
-        </nav>
+          <SignedOut>
+        <SignInButton>
+          <Button variant="outline">Sign In</Button>
+        </SignInButton>
+      </SignedOut>
 
+      <SignedIn>
+        <UserButton 
+        appearance={{
+          elements: {
+            avatarBox: " w-10 h-10",
+            userButtonPopoverCard: "shadow-xl",
+            userPreviewMainIdentifier: "font-semibold",
+          },
+        }}
+          afterSignOutUrl="/"
+        />
+      </SignedIn>
+        </div>
+      </nav>
 
-         <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+      {/* Auth Buttons and UserButton */}
+      
     </header>
   )
 }
